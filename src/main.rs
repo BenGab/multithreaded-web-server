e3565c607caad5431cd225d921020ce1aa5ea5d7
Gabor Benko <benkogabor87@gmail.com>
2024-03-28 19:43:08 +0100

    reading request

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use std::net::TcpListener;
+use std::{io::{BufRead, BufReader}, net::{TcpListener, TcpStream}};
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
 
     for stream in listener.incoming() {
-        let stream = stream?;
-
-        println!("Connection established !");
+        handle_connection(stream?)
     }
 
     Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+                        .lines()
+                        .map(|result| result.unwrap())
+                        .take_while(|line| !line.is_empty())
+                        .collect();
+    
+    println!("Request: {:#?}", http_request);
 }
