a90295629894d403d9f8ebf4070537687948940d
Gabor Benko <benkogabor87@gmail.com>
2024-04-03 18:34:17 +0200

    render dummy html content

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
@@ -18,6 +18,9 @@ fn handle_connection(mut stream: TcpStream) {
                         .take_while(|line| !line.is_empty())
                         .collect();
     
-    let response = "HTTP/1.1 200 Ok\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
